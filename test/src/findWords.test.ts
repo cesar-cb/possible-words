0973c7fa5d1a974c3281055923bfd7705fc4231b
Cesar Boaventura <cesar.boaventura61@gmail.com>
2022-02-22 12:43:45 -0300

    test: add en test case

--- test/src/findWords.test.ts
+++ test/src/findWords.test.ts
@@ -1,17 +1,24 @@
 import findWords from 'src/findWords';
 
-const language: 'pt-br' | 'en' = 'pt-br';
-
-const params = {
-  word: 'car?a',
-  exclude: [],
-  has: ['g'],
-  language,
+type TParams = {
+  language?: 'pt-br' | 'en';
 };
 
+const params = ({ language = 'pt-br' }: TParams = {}) => ({
+  word: 'paus?',
+  exclude: [],
+  has: [],
+  language,
+});
+
 describe('findWords', () => {
-  it('should return an array of words', () => {
-    const words = findWords(params);
-    expect(words).toEqual(['carga']);
+  it('should return an array of words [pt]', () => {
+    const words = findWords(params());
+    expect(words).toEqual(['pausa']);
+  });
+
+  it('should return an array of words [en]', () => {
+    const words = findWords(params({ language: 'en' }));
+    expect(words).toEqual(['pause']);
   });
 });
